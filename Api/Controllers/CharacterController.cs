e69c031de51c45d745080d305e296c1866212db1
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 20:52:35 -0300

    Metodo para Obtener lso Personajes

--- Api/Controllers/CharacterController.cs
+++ Api/Controllers/CharacterController.cs
@@ -21,5 +21,48 @@ namespace Api.Controllers
         {
             _configuration = configuration;
         }
+        [HttpGet]
+        [Route("Characters")]
+        public JsonResult ObtenerPersonajes()
+        {
+            var resultado = new CharacterResultado();
+            try
+            {
+
+                //genero un string con la consulta hacia la BD
+                string Consulta = @"SELECT Nombre,imagen
+                                    FROM Personajes;";
+                //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+                DataTable tb = new DataTable();
+                //creo una variable reader para capturar los datos
+                SqlDataReader MyReader;
+                //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+                string sqlDataSource = _configuration.GetConnectionString("BD");
+
+                using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                {
+
+                    sqlcon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                    {
+                        MyReader = myCommand.ExecuteReader();
+                        //la tabla la cargo con los datos obtenidos de mi select 
+                        tb.Load(MyReader);
+                        //cierro las conexiones
+                        MyReader.Close();
+                        sqlcon.Close();
+                        resultado.Personajes = tb;
+                        return new JsonResult(resultado.Personajes);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado.Error = "Ocurrio un Error Inesperado";
+                resultado.InfoAdicional = ex.ToString();
+                return new JsonResult(resultado.Error + resultado.InfoAdicional);
+            }
+        }
+        
     }
 }
