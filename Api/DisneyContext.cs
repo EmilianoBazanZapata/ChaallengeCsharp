76ae75cc7d9242972a4fc606fddacd0f0bd1afb3
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 20:06:29 -0300

    Agregando el Context

---
+++ Api/DisneyContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api
+{
+    public class DisneyContext : DbContext
+    {
+        public DisneyContext(DbContextOptions<DisneyContext> options ):base(options)
+        {
+
+        }
+        public DbSet<Personaje> Personajes {get;set;}
+        public DbSet<Genero> Generos {get;set;}
+        public DbSet<Pelicula> Peliculas {get;set;}
+
+    }
+}
