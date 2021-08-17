de5e66b11ad8da436ffc24b90a915fee478c9a43
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 22:14:35 -0300

    Metodo para Buscar un Personaje por el nombre

--- Api/Controllers/CharacterController.cs
+++ Api/Controllers/CharacterController.cs
@@ -258,6 +258,48 @@ namespace Api.Controllers
             }
             
         }
+        [HttpGet]
+        [Route("SearchCharacterForName/characters/{name}")]
+        public JsonResult BuscarPersonajePorNombre(string name)
+        {
+            var resultado = new CharacterResultado();
+            try
+            {
+
+                //genero un string con la consulta hacia la BD
+                string Consulta = @"SELECT * FROM V_PER(@NOMBRE)";
+                //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+                DataTable tb = new DataTable();
+                //creo una variable reader para capturar los datos
+                SqlDataReader MyReader;
+                //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+                string sqlDataSource = _configuration.GetConnectionString("BD");
+
+                using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                {
+
+                    sqlcon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                     {
+                        myCommand.Parameters.AddWithValue("@NOMBRE", name);
+                        MyReader = myCommand.ExecuteReader();
+                        //la tabla la cargo con los datos obtenidos de mi select 
+                        tb.Load(MyReader);
+                        //cierro las conexiones
+                        MyReader.Close();
+                        sqlcon.Close();
+                        resultado.Personajes = tb;
+                        return new JsonResult(resultado.Personajes);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado.Error = "Ocurrio un Error Inesperado";
+                resultado.InfoAdicional = ex.ToString();
+                return new JsonResult(resultado.Error + resultado.InfoAdicional);
+            }
+        }
         
     }
 }
