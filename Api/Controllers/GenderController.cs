86ad151981815f7b37edad1b6b63517387cf7b76
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 22:19:59 -0300

    Agregando el Metodo para Ver los Generos

--- Api/Controllers/GenderController.cs
+++ Api/Controllers/GenderController.cs
@@ -20,5 +20,48 @@ namespace Api.Controllers
         {
             _configuration = configuration;
         }
+        [HttpGet]
+        [Route("Genders")]
+        public JsonResult ObtenerGeneros()
+        {
+            var resultado = new GenderResultado();
+            try
+            {
+
+                //genero un string con la consulta hacia la BD
+                string Consulta = @"SELECT Nombre,imagen
+                                    FROM Generos;";
+                //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+                DataTable tb = new DataTable();
+                //creo una variable reader para capturar los datos
+                SqlDataReader MyReader;
+                //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+                string sqlDataSource = _configuration.GetConnectionString("BD");
+
+                using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                {
+
+                    sqlcon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                    {
+                        MyReader = myCommand.ExecuteReader();
+                        //la tabla la cargo con los datos obtenidos de mi select 
+                        tb.Load(MyReader);
+                        //cierro las conexiones
+                        MyReader.Close();
+                        sqlcon.Close();
+                        resultado.Generos = tb;
+                        return new JsonResult(resultado.Generos);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado.Error = "Ocurrio un Error Inesperado";
+                resultado.InfoAdicional = ex.ToString();
+                return new JsonResult(resultado.Error + resultado.InfoAdicional);
+            }
+        }
+        
     }
 }
