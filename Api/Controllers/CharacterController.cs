ff4be71e10851e115340954f1ef13ec7e2acc4d4
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 22:11:32 -0300

    Agregando el Metodo para Actualizar un Personaje

--- Api/Controllers/CharacterController.cs
+++ Api/Controllers/CharacterController.cs
@@ -64,7 +64,6 @@ namespace Api.Controllers
                 return new JsonResult(resultado.Error + resultado.InfoAdicional);
             }
         }
-    
         [HttpPost]
         [Route("Character/AddCharacter")]
         public JsonResult AgregarPersonaje([FromBody] ComandoAgregarPersonaje personaje)
@@ -128,6 +127,78 @@ namespace Api.Controllers
                             MyReader.Close();
                             sqlcon.Close();
                             resultado.InfoAdicional = "Se Agrego el personaje Exitosamente";
+                            return new JsonResult(resultado.InfoAdicional);
+                            
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado.Error = "Ocurrio un Error Inesperado";
+                    resultado.InfoAdicional = ex.ToString();
+                    return new JsonResult(resultado.Error + resultado.InfoAdicional);
+                }
+            }
+        }
+        [HttpPut]
+        [Route("Character/UpdateCharacter")]
+        public JsonResult ActualizarPersonaje([FromBody] ComandoActualizarPersonaje personaje)
+        {
+            var resultado = new CharacterResultado();
+            //genero un string con la consulta hacia la BD
+            string Consulta = @"EXEC UP_ACTUALIZAR_PERSONAJE @NOMBRE, @EDAD, @PESO, @HISTORIA, @IMAGEN, @ID";
+            //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+            DataTable tb = new DataTable();
+            //creo una variable reader para capturar los datos
+            SqlDataReader MyReader;
+            //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+            //string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+            string sqlDataSource = _configuration.GetConnectionString("BD");
+
+            if (personaje.Nombre.Equals(""))
+            {
+                resultado.Error = "Debe Ingreesar un Nombre";
+                return new JsonResult(resultado.Error);
+            }
+            if (personaje.Edad.Equals(""))
+            {
+                resultado.Error = "Debe Ingreesar una Edad";
+                return new JsonResult(resultado.Error);
+            }
+            if (personaje.Peso.Equals(""))
+            {
+                resultado.Error = "Debe Ingreesar un Peso";
+                return new JsonResult(resultado.Error);
+            }
+            if (personaje.Historia.Equals(""))
+            {
+                resultado.Error = "Debe Ingreesar una Historia";
+                return new JsonResult(resultado.Error);
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                    {
+
+                        sqlcon.Open();
+                        using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                        {
+                            myCommand.Parameters.AddWithValue("@NOMBRE", personaje.Nombre);
+                            myCommand.Parameters.AddWithValue("@EDAD", personaje.Edad);
+                            myCommand.Parameters.AddWithValue("@PESO", personaje.Peso);
+                            myCommand.Parameters.AddWithValue("@HISTORIA", personaje.Historia);
+                            myCommand.Parameters.AddWithValue("@IMAGEN", personaje.Imagen);
+                            myCommand.Parameters.AddWithValue("@ID", personaje.Id);
+                            MyReader = myCommand.ExecuteReader();
+                            //la tabla la cargo con los datos obtenidos de mi sentencia
+                            tb.Load(MyReader);
+                            //cierro las conexiones
+                            MyReader.Close();
+                            sqlcon.Close();
+                            resultado.InfoAdicional = "El Personaje se Actualizo Exitosamente";
                             return new JsonResult(resultado.InfoAdicional);
                             
                         }
