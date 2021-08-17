9305bd5ef04f410df41467329725383d6225ca5e
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 22:32:25 -0300

    Agregando el Metodo para Buscar personajes desde una Pelicula

--- Api/Controllers/MovieController.cs
+++ Api/Controllers/MovieController.cs
@@ -382,6 +382,48 @@ namespace Api.Controllers
             }
             
         }
+        [HttpGet]
+        [Route("SearchCharacterForMovie/Movie/{IdMovie}")]
+        public JsonResult BuscarPersonajePorPelicula(int IdMovie)
+        {
+            var resultado = new CharacterResultado();
+            try
+            {
+
+                //genero un string con la consulta hacia la BD
+                string Consulta = @" SELECT * FROM V_VER_ACTORES_DE_UNA_PELICULA (@IDPELICULA)";
+                //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+                DataTable tb = new DataTable();
+                //creo una variable reader para capturar los datos
+                SqlDataReader MyReader;
+                //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+                string sqlDataSource = _configuration.GetConnectionString("BD");
+
+                using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                {
+
+                    sqlcon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                     {
+                        myCommand.Parameters.AddWithValue("@IDPELICULA", IdMovie);
+                        MyReader = myCommand.ExecuteReader();
+                        //la tabla la cargo con los datos obtenidos de mi select 
+                        tb.Load(MyReader);
+                        //cierro las conexiones
+                        MyReader.Close();
+                        sqlcon.Close();
+                        resultado.Personajes = tb;
+                        return new JsonResult(resultado.Personajes);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado.Error = "Ocurrio un Error Inesperado";
+                resultado.InfoAdicional = ex.ToString();
+                return new JsonResult(resultado.Error + resultado.InfoAdicional);
+            }
+        }
         
     }
 }
