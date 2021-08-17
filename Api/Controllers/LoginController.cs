d9e45d2246a1a5d0584176dfdc6655e3f3ac6301
maximolicanelo <maximolicanelo@gmail.com>
2021-08-17 10:34:03 -0300

    Agregando el Metodo para registrar un usuario

--- Api/Controllers/LoginController.cs
+++ Api/Controllers/LoginController.cs
@@ -3,6 +3,8 @@ using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using API.Comandos.ComandosPelicula;
+using Resultado;
 
 namespace Api.Controllers
 {
@@ -14,5 +16,72 @@ namespace Api.Controllers
         {
             _configuration = configuration;
         }
+        [HttpPost]
+        [Route("User/AddUser")]
+        public JsonResult AgregarPersonaje([FromBody] ComandoRegistrarUsuario usuario)
+        {
+            var resultado = new LoginResultado();
+            //genero un string con la consulta hacia la BD
+                                                          
+
+            string Consulta = @"EXEC UP_REGISTRAR_USUARIO @EMAIL, @PASSWORD";
+            //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+            DataTable tb = new DataTable();
+            //creo una variable reader para capturar los datos
+            SqlDataReader MyReader;
+            //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+            string sqlDataSource = _configuration.GetConnectionString("BD");
+
+            if (usuario.Email.Equals(""))
+            {
+                resultado.Error = "Debe Ingresar un Email";
+                return new JsonResult(resultado.Error);
+            }
+            if (usuario.Password.Equals(""))
+            {
+                resultado.Error = "Debe Ingresar una Contraseña";
+                return new JsonResult(resultado.Error);
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                    {
+
+                        sqlcon.Open();
+                        using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                        {
+                            if(resultado.VerificarUsuario(sqlcon,usuario.Email,usuario.Password))
+                            {
+                                resultado.InfoAdicional = "Estos Datos Estan Registrados";
+                                return new JsonResult(resultado.InfoAdicional);
+                            }
+                            else
+                            {
+                                myCommand.Parameters.AddWithValue("@EMAIL", usuario.Email);
+                                myCommand.Parameters.AddWithValue("@PASSWORD", usuario.Password);
+                                MyReader = myCommand.ExecuteReader();
+                                //la tabla la cargo con los datos obtenidos de mi sentencia
+                                tb.Load(MyReader);
+                                //cierro las conexiones
+                                MyReader.Close();
+                                sqlcon.Close();
+                                resultado.InfoAdicional = "Se Registro el Usuario Exitosamente";
+                                return new JsonResult(resultado.InfoAdicional);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado.Error = "Ocurrio un Error Inesperado";
+                    resultado.InfoAdicional = ex.ToString();
+                    return new JsonResult(resultado.Error + resultado.InfoAdicional);
+                }
+            }
+        }
+        
+        
     }
 }
--- Api/Resultados/LoginResultado.cs
+++ Api/Resultados/LoginResultado.cs
@@ -1,6 +1,6 @@
-using System.Data;
 using System;
-using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace Resultado
 {
@@ -10,5 +10,24 @@ namespace Resultado
         public string Error { get; set; }
         public string InfoAdicional { get; set; }
         public DataTable Usuario { get; set; }
+        public bool VerificarUsuario(SqlConnection sqlcon, string Email, string Password)
+        {
+            var verificar = string.Format("SELECT * FROM V_VERIFICAR_EXISTENCIA_DE_USUARIO(@EMAIL,@PASSWORD)");
+            SqlCommand comando = new SqlCommand(verificar, sqlcon);
+            comando.Parameters.AddWithValue("@EMAIL", Email);
+            comando.Parameters.AddWithValue("@PASSWORD", Password);
+            SqlDataReader dr = null;
+            dr = comando.ExecuteReader();
+            if (dr.Read())
+            {
+                dr.Close();
+                return true;
+            }
+            else
+            {
+                dr.Close();
+                return false;
+            }
+        }
     }
 }
