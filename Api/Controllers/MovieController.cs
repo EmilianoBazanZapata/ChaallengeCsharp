99243cd297fcdd73a2053eb9ecb81c389a173edf
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 22:25:47 -0300

    Agregando el Metodo para ver las Peliculas Agregadas

--- Api/Controllers/MovieController.cs
+++ Api/Controllers/MovieController.cs
@@ -21,5 +21,48 @@ namespace Api.Controllers
         {
             _configuration = configuration;
         }
+        [HttpGet]
+        [Route("Movies")]
+        public JsonResult ObtenerPeliculasActivas()
+        {
+            var resultado = new MovieResultado();
+            try
+            {
+
+                //genero un string con la consulta hacia la BD
+                string Consulta = @"SELECT Titulo, imagen , FechaCreacion
+                                    FROM Peliculas;";
+                //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+                DataTable tb = new DataTable();
+                //creo una variable reader para capturar los datos
+                SqlDataReader MyReader;
+                //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+                string sqlDataSource = _configuration.GetConnectionString("BD");
+
+                using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                {
+
+                    sqlcon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                    {
+                        MyReader = myCommand.ExecuteReader();
+                        //la tabla la cargo con los datos obtenidos de mi select 
+                        tb.Load(MyReader);
+                        //cierro las conexiones
+                        MyReader.Close();
+                        sqlcon.Close();
+                        resultado.Peliculas = tb;
+                        return new JsonResult(resultado.Peliculas);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado.Error = "Ocurrio un Error Inesperado";
+                resultado.InfoAdicional = ex.ToString();
+                return new JsonResult(resultado.Error + resultado.InfoAdicional);
+            }
+        }
+        
     }
 }
