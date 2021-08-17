2c5b37211e21b792cfb07b1d755ecb77a7d115be
maximolicanelo <maximolicanelo@gmail.com>
2021-08-16 22:29:40 -0300

    Agregando el Metodo para Ordenar las Peliculas de Forma Descendente

--- Api/Controllers/MovieController.cs
+++ Api/Controllers/MovieController.cs
@@ -251,6 +251,48 @@ namespace Api.Controllers
             }
             
         }
+        [HttpGet]
+        [Route("SearchMovieDesc")]
+        public JsonResult OrdenarPeliculasDeFormaDescendente()
+        {
+            var resultado = new CharacterResultado();
+            try
+            {
+                //genero un string con la consulta hacia la BD
+                string Consulta = @"SELECT *
+                                    FROM peliculas P
+                                    ORDER BY P.FechaCreacion DESC";
+                //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+                DataTable tb = new DataTable();
+                //creo una variable reader para capturar los datos
+                SqlDataReader MyReader;
+                //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+                string sqlDataSource = _configuration.GetConnectionString("BD");
+
+                using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+                {
+
+                    sqlcon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                     {
+                        MyReader = myCommand.ExecuteReader();
+                        //la tabla la cargo con los datos obtenidos de mi select 
+                        tb.Load(MyReader);
+                        //cierro las conexiones
+                        MyReader.Close();
+                        sqlcon.Close();
+                        resultado.Personajes = tb;
+                        return new JsonResult(resultado.Personajes);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado.Error = "Ocurrio un Error Inesperado";
+                resultado.InfoAdicional = ex.ToString();
+                return new JsonResult(resultado.Error + resultado.InfoAdicional);
+            }
+        }
         
     }
 }
