5d8a38904ec70d1cf3a1af740210001f6c3e7923
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-23 10:21:43 -0300

    Agergando el Metodo para listar las Peliculas

--- NuevaVersion/Challenge Disney/Api/Controllers/PeliculaController.cs
+++ NuevaVersion/Challenge Disney/Api/Controllers/PeliculaController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.Data;
+using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Controllers
 {
-    public class PeliculaController
+    public class PeliculaController : ControllerBase
     {
         private readonly DisneyContext _db;
         public PeliculaController(DisneyContext db)
         {
             _db = db;
+        }
+        [HttpGet]
+        [Route("Movies")]
+        public async Task<IActionResult> GetPeliculas()
+        {
+            //ordeno las Peliculas por nombre
+            var lista = await _db.Peliculas.Where(p => p.Activo == true).OrderBy(p => p.Titulo).ToListAsync();
+            return Ok(lista);
         }
     }
 }
