e2fdf61238a1f583b2695700726c2e73d8d16abf
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-23 09:26:08 -0300

    Listando los Géneros Disponibles

--- NuevaVersion/Challenge Disney/Api/Controllers/GeneroController.cs
+++ NuevaVersion/Challenge Disney/Api/Controllers/GeneroController.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.Data;
+using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
-    public class GeneroController
+    public class GeneroController : ControllerBase
     {
         private readonly DisneyContext _db;
         public GeneroController(DisneyContext db)
         {
             _db = db;
         }
+        [HttpGet]
+        [Route("Genders")]
+        public async Task<IActionResult> GetGeneros()
+        {
+            //ordeno los generos por nombre
+            var lista = await _db.Generos.OrderBy(p => p.Nombre).ToListAsync();
+            return Ok(lista);
+        }
+        
     }
 }
