8ba6f4d47d3089cd472c91bcfb618aa9cf85b3db
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-24 01:29:33 -0300

    Enviando un Email de Bienvenida a los nuevos usuarios

--- NuevaVersion/Challenge Disney/Api/Controllers/LoginController.cs
+++ NuevaVersion/Challenge Disney/Api/Controllers/LoginController.cs
@@ -6,9 +6,11 @@ using Api.Data;
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using EASendMail;
+
 namespace Api.Controllers
 {
-    public class LoginController: ControllerBase
+    public class LoginController : ControllerBase
     {
         private readonly DisneyContext _db;
         public LoginController(DisneyContext db)
@@ -32,8 +34,37 @@ namespace Api.Controllers
             await _db.AddAsync(usuario);
             //por ultimo gurado los cambios
             await _db.SaveChangesAsync();
+
+            string email = "";
+            string contraseña = "";
+            SmtpMail objeto_correo = new SmtpMail("TryIt");
+            //desde donde lo envio
+            objeto_correo.From = "Disney";
+            //hacia donde lo envio
+            objeto_correo.To = usuario.Email;
+            //asunto
+            objeto_correo.Subject = "Bienvenido a Disney : " + usuario.Email;
+            //mensaje
+            objeto_correo.TextBody = "si logras ver este mensaje es que pudiste registrarte con existo a nuestro sistema";
+
+            SmtpServer objetoservidor = new SmtpServer("smtp.gmail.com");
+
+            //mail con el que me logeo
+            objetoservidor.User = email;
+            objetoservidor.Password = contraseña;
+
+            //puerto que uso
+            objetoservidor.Port = 587;
+            //conexion que voy a usar y la cifro
+            objetoservidor.ConnectType = SmtpConnectType.ConnectSSLAuto;
+
+            //poder enviar el correo
+            EASendMail.SmtpClient objetocliente = new EASendMail.SmtpClient();
+            objetocliente.SendMail(objetoservidor, objeto_correo);
+
+
             return Ok();
         }
-        
+
     }
 }
