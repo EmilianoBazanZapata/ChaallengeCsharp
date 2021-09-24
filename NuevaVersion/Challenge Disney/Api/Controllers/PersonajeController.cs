81e11241f5323621aa3923cd89621b66df171383
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-23 22:19:56 -0300

    Agregando el Metodo par alistar personajes

--- NuevaVersion/Challenge Disney/Api/Controllers/PersonajeController.cs
+++ NuevaVersion/Challenge Disney/Api/Controllers/PersonajeController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.Data;
+using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Controllers
 {
-    public class PersonajeController
+    public class PersonajeController : ControllerBase
     {
         private readonly DisneyContext _db;
         public PersonajeController(DisneyContext db)
         {
             _db = db;
         }
+        [HttpGet]
+        [Route("Characters/ListCharacters")]
+        public async Task<IActionResult> GetCharacters()
+        {
+            //ordeno los personajes por nombre
+            var lista = await _db.Personajes.Where(p => p.Activo == true).OrderBy(p => p.Nombre).ToListAsync();
+            return Ok(lista);
+        }
+
+
     }
 }
